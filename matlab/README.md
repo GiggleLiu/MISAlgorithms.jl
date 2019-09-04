0d7d7ac254836bd4883107b483705a753eb2c12f
Wang-Shengtao <shengtaowst@gmail.com>
2019-09-04 10:23:20 -0400

    benchmarking instances for UDG
    update UnitDiskGraph.m

--- matlab/README.md
+++ matlab/README.md
@@ -1,3 +1,24 @@
 ## Create unit disk graphs
 
 Use UnitDiskGraph(N, rho,1) to create a unit disk graph with N vertices, density rho, and periodic boundary condition. 
+
+## Benchmarking instances
+
+Data Files for benchmarking in the paper arXiv:1808.10816
+1) Fig. 3a - UDGp-instances-for-phasediag.mat
+2) Fig. 3b - UDGp2-instances-for-phasediag-line.mat
+
+The variable xyCoords contains the x-y coordinates of vertices.
+xyCoords{i,j,k} gives the coordinates for a graph with
+	size N = NvertToTry(i)
+	density rho = rhos(j)
+	instance # k
+
+Actual graph edge list and adjacency matrix can be generated using UnitDiskGraph.m
+The benchmarking are done assuming the vertex are placed on a torus of size sqrt(N/rho) x sqrt(N/rho), i.e. square with periodic boundary condition.
+
+For example, use
+
+	[edges, A] = UnitDiskGraph(xyCoords{i,j,k}, rhos(j), 1)
+
+to generate edge list 'edges' and adjacency matrix 'A'
--- matlab/UnitDiskGraph.m
+++ matlab/UnitDiskGraph.m
@@ -6,6 +6,11 @@ function [edges, AdjMat, xy] = UnitDiskGraph(varargin)
 %       generates unit disk graph from coordinates of vertices, where any
 %       two vertices with <= 1 distance are connected by an edge
 %
+%   [edges, AdjMat, xy] = UnitDiskGraph(xy, rho, 1)
+%       generates unit disk graph from coordinates of vertices, where any
+%       two vertices with <= 1 distance on a LxL torus [L=sqrt(N/rho)] are
+%       connected by an edge [N = size(xy,1)]
+%
 %   [edges, AdjMat, xy] = UnitDiskGraph(N, rho)
 %       generates unit disk graph of N vertices with target density rho (open boundaries) 
 % 
@@ -20,19 +25,24 @@ function [edges, AdjMat, xy] = UnitDiskGraph(varargin)
 %           xy = coordinates of vertices
 
 
-if nargin == 1
-    xy = varargin{1};
-    NNodes = size(xy,1);
-else
+if numel(varargin{1}) == 1
     NNodes = varargin{1};
     rho = varargin{2};
     L = sqrt(NNodes/rho);
     xy = rand(NNodes, 2)*L;
+else
+    xy = varargin{1};
+    NNodes = size(xy,1);
+    
+    if nargin >= 2
+        rho = varargin{2};
+        L = sqrt(NNodes/rho);
+    end
 end
 
 AdjMat = zeros(NNodes);
 
-if nargin == 2 || (nargin == 3 && varargin{3} == 0) % open boundary condition
+if nargin <= 2 || (nargin == 3 && varargin{3} == 0) % open boundary condition
     for ind = 1:NNodes
         myXY = repmat(xy(ind, :), NNodes, 1);
         dist = sum((xy - myXY).^2,2);
