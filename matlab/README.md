ab9b965d54ececb915c27f57fcb2f23dc2b0f7ee
Wang-Shengtao <shengtaowst@gmail.com>
2019-09-03 20:23:34 -0400

    generate unit disk graphs

---
+++ matlab/README.md
@@ -0,0 +1,3 @@
+## Create unit disk graphs
+
+Use UnitDiskGraph(N, rho,1) to create a unit disk graph with N vertices, density rho, and periodic boundary condition. 
---
+++ matlab/UnitDiskGraph.m
@@ -0,0 +1,56 @@
+function [edges, AdjMat, xy] = UnitDiskGraph(varargin)
+%UnitDiskGraph creates unit disk graph
+%
+%   Usages:
+%   [edges, AdjMat, xy] = UnitDiskGraph(xy)
+%       generates unit disk graph from coordinates of vertices, where any
+%       two vertices with <= 1 distance are connected by an edge
+%
+%   [edges, AdjMat, xy] = UnitDiskGraph(N, rho)
+%       generates unit disk graph of N vertices with target density rho (open boundaries) 
+% 
+%   [edges, AdjMat, xy] = UnitDiskGraph(N, rho,0)
+%       generates unit disk graph of N vertices with target density rho (open boundaries)
+%
+%   [edges, AdjMat, xy] = UnitDiskGraph(N, rho,1)
+%       generates unit disk graph of N vertices with target density rho (periodic boundaries)
+%
+%   Output: edges = list of edges (i, j)
+%           AdjMat = adjacency matrix
+%           xy = coordinates of vertices
+
+
+if nargin == 1
+    xy = varargin{1};
+    NNodes = size(xy,1);
+else
+    NNodes = varargin{1};
+    rho = varargin{2};
+    L = sqrt(NNodes/rho);
+    xy = rand(NNodes, 2)*L;
+end
+
+AdjMat = zeros(NNodes);
+
+if nargin == 2 || (nargin == 3 && varargin{3} == 0) % open boundary condition
+    for ind = 1:NNodes
+        myXY = repmat(xy(ind, :), NNodes, 1);
+        dist = sum((xy - myXY).^2,2);
+        AdjMat(dist <= 1, ind) = 1;
+        AdjMat(ind, ind) = 0;
+    end
+end
+
+if nargin == 3 && varargin{3} == 1 % periodic boundary condition
+    for ind = 1:NNodes
+        myXY = repmat(xy(ind, :), NNodes, 1);
+        dist = mod(xy-myXY,L);
+        dist = min(dist,L-dist); % minimum distance
+        dist = sum(dist.^2,2);
+        AdjMat(dist <= 1, ind) = 1;
+        AdjMat(ind, ind) = 0;
+    end
+end
+
+[r, c] = find(triu(AdjMat));
+edges = [r, c];
